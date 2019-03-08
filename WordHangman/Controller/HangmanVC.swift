e5bee7334ef7441368d1f595e214817d77d6fdcf
tarek bahie <you@example.com>
2019-03-08 20:19:02 +0200

    UI + character logic implemented

---
+++ WordHangman/Controller/HangmanVC.swift
@@ -0,0 +1,162 @@
+//
+//  HangmanVC.swift
+//  WordHangman
+//
+//  Created by tarek bahie on 3/8/19.
+//  Copyright © 2019 tarek bahie. All rights reserved.
+//
+
+import UIKit
+
+class HangmanVC: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var wordLbl: UILabel!
+    @IBOutlet weak var letterTxtField: UITextField!
+    @IBOutlet weak var remainingGuessLbl: UILabel!
+    @IBOutlet weak var hintLbl: UILabel!
+    @IBOutlet weak var choosenLettersLbl: UITextView!
+    @IBOutlet weak var restartBtn: UIButton!
+    @IBOutlet weak var hangmanImg: UIImageView!
+    
+    
+    let words = ["Cairo","Real Madrid","Gravity","Batman","Sun","Moon","Egypt","Iphone","Macbook","Spain"]
+    let hints = ["an african capital","a spanish club","a force of nature","a DC comics hero","a bright star","seen only at night","an african country","a handheld apple product","an apple device","a european country"]
+    var choosenWord = ""
+    var index = 0
+    var wordAsUnderscores = ""
+    var wordToGuess = ""
+    var maxGuess = 7
+    var newInt: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        choosenLettersLbl.isUserInteractionEnabled = false
+        chooseWordToStartGame()
+        wordLbl.text = wordAsUnderscores
+        remainingGuessLbl.text = "\(maxGuess) guesses left"
+        letterTxtField.delegate = self
+
+    }
+    @IBAction func restartBtnPressed(_ sender: Any) {
+        wordToGuess = ""
+        maxGuess = 7
+        remainingGuessLbl.text = "\(maxGuess) guesses left"
+        wordAsUnderscores = ""
+        hangmanImg.isHidden = true
+        chooseWordToStartGame()
+        wordLbl.text = wordAsUnderscores
+        letterTxtField.isEnabled = true
+        print(wordToGuess)
+        print(wordAsUnderscores)
+    }
+    
+    // MARK: - Word Manipulation
+    
+    func chooseWordToStartGame() {
+        choosenLettersLbl.text = ""
+        index = createRandomInt(oldInt: newInt)
+        hintLbl.text = "Hint: " + hints[index]
+        wordToGuess = words[index].lowercased()
+        turnStringToUnderscores()
+    }
+    func turnStringToUnderscores() {
+        var myIndex = wordToGuess.startIndex
+        while myIndex < wordToGuess.endIndex {
+            for _ in 0..<wordToGuess.count {
+                if wordToGuess[myIndex] == " " {
+                    wordAsUnderscores.append("  ")
+                    myIndex = wordToGuess.index(after: myIndex)
+                } else {
+                    wordAsUnderscores.append("_")
+                    myIndex = wordToGuess.index(after: myIndex)
+                }
+            }
+            
+        }
+    }
+    func createRandomInt(oldInt : Int) -> Int {
+         newInt = Int.random(in: 0..<10)
+        if newInt == oldInt {
+            newInt = Int.random(in: 0..<10)
+        }
+        return newInt
+        
+    }
+    // MARK: - TextFieldDelegate
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let letterGuessed = textField.text else { return }
+        letterTxtField.text?.removeAll()
+        let currentLetterBank : String = choosenLettersLbl.text ?? ""
+        if currentLetterBank.contains(letterGuessed) {
+            return
+        } else {
+            if wordToGuess.contains(letterGuessed) {
+                processCorrectGuess(letterGuessed: letterGuessed)
+            } else {
+                processIncorrectGuess()
+                choosenLettersLbl.text?.append("\(letterGuessed), ")
+            }
+            
+        }
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let allowedCharacters = NSCharacterSet.lowercaseLetters
+        let startingLength = textField.text?.count ?? 0
+        let lengthToAdd = string.count
+        let lengthToReplace = range.length
+        let newLength = startingLength + lengthToAdd - lengthToReplace
+        
+        if string.isEmpty {
+            return true
+        } else if newLength == 1 {
+            if let _ = string.rangeOfCharacter(from: allowedCharacters, options: .caseInsensitive) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    
+    // MARK: - character analysis
+    func processCorrectGuess(letterGuessed: String) {
+        let characterGuessed = Character(letterGuessed)
+        var myIndex = wordToGuess.startIndex
+        while myIndex < wordToGuess.endIndex {
+            for _ in 0..<wordToGuess.count {
+                if wordToGuess[myIndex] == characterGuessed {
+                    let endIndex = wordToGuess.index(after: myIndex)
+                    let charRange = myIndex..<endIndex
+                    wordAsUnderscores = wordAsUnderscores.replacingCharacters(in: charRange, with: letterGuessed)
+                    
+                }
+                myIndex = wordToGuess.index(after: myIndex)
+                wordLbl.text = wordAsUnderscores
+            }
+        }
+        if !(wordAsUnderscores.contains("_")) {
+            remainingGuessLbl.text = "You win! :)"
+            letterTxtField.isEnabled = false
+        }
+        
+    }
+    func processIncorrectGuess() {
+        maxGuess -= 1
+        hangmanImg.isHidden = false
+        if maxGuess == 0 {
+            hangmanImg.image = UIImage(named: "Unknown")
+            remainingGuessLbl.text = "You lost! the correct answer is: \(wordToGuess)"
+            wordLbl.text = ""
+            letterTxtField.isEnabled = false
+        } else {
+            remainingGuessLbl.text = "\(maxGuess) guesses left"
+            hangmanImg.image = UIImage(named: "Unknown\(maxGuess)")
+        }
+    }
+}
--- WordHangman/Controller/MainVC.swift
+++ WordHangman/Controller/MainVC.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func newGameBtnPressed(_ sender: Any) {
+        performSegue(withIdentifier: "toNewGame", sender: self)
+    }
+    
 }
 
